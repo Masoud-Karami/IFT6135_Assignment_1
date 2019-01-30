cf7f5776599c4eb7bdff295fefba3fa414128f07
Mathieu Champagne <mathcham@hotmail.com>
2019-01-30 15:11:24 -0500

    First commit: added the basic framework

---
+++ assignment1.py
@@ -0,0 +1,33 @@
+class NN(object):
+    
+    def __init__(self,hidden_dims=(1024,2048),n_hidden=2,mode='train',datapath=None,model_path=None):
+	
+	
+	def initialize_weights(self,n_hidden,dims):
+	
+	
+	def forward(self,input,labels,..):
+	
+	
+	def activation(self,input):
+	
+	
+	def loss(self,prediction,..):
+	
+	
+	def softmax(self,input,..):
+	
+	
+	def backward(self,cache,labels,...):
+	
+	
+	def update(self,grads,..):
+	
+	
+	def train(self):
+	
+	
+	def test(self):
+	
+	
+	
