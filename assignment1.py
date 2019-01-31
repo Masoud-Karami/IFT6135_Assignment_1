c9107ce107bf5d77cc7523fb6f73d70ee4d029a9
Mathieu Champagne <mathcham@hotmail.com>
2019-01-31 11:24:49 -0500

    Added a display function to visualize the weights and structure of NN & also a save/load function

--- assignment1.py
+++ assignment1.py
@@ -6,15 +6,21 @@
 # UdeM, IFT6135, Assignment 1, H2019
 
 import numpy as np
+np.set_printoptions(precision=2)
 import random
+import pickle
 
 class NN(object):
     
-    def __init__(self,dims=(2,2,2,2),n_hidden=2,mode='train',datapath=None,model_path=None): #dims=(784,1024,2048,10)
+    def __init__(self,dims=(784,1024,2048,10),n_hidden=2,mode='train',datapath=None,model_path=None):
         
-        self.layers = []
-        self.initialize_weights(n_hidden,dims)
-        self.cool = []
+        if model_path is None:
+            self.dims = dims
+            self.n_hidden = n_hidden
+            self.layers = []
+            self.initialize_weights(n_hidden,dims)
+        else:
+            self.load(model_path)
     
     def initialize_weights(self,n_hidden,dims):
 	# either ZERO init / NORMAL DISTRIBUTION init / GLOROT init
@@ -24,11 +30,9 @@ class NN(object):
             for n in range(len(self.layers[l].neurons)):
                 for w in range(dims[l]):
                     self.layers[l].neurons[n].weights.append(random.random()) #here instead of random, put the normal random function or whatever else
-                #self.layers[l].neurons[n].display() #to visualize the weight of each neurons
     
     def forward(self,input,labels):#..
 	# forward propagation of the NN (use activation functions)
-	# need to figure out how to store the weights/bias (vector? list? vector of vectors?)
         print("")
         
     def activation(self,input):
@@ -73,24 +77,64 @@ class NN(object):
 	# test the non-training dataset and output the accuracy rate...
     	print("")
         
+    def save(self,filename):
+        # saves the weights and structure of the current NN
+        with open(filename, 'wb') as f:
+            pickle.dump([self.dims, self.n_hidden, self.layers], f)
+            
+    def load(self, filename):
+        # load the weights and structure of the saved NN
+        with open(filename, 'rb') as f:
+            self.dims, self.n_hidden, self.layers = pickle.load(f)
+    
+    def display(self, display_weights=False):
+        strings = ["#inputs"]
+        for i in range(1,self.n_hidden+1):
+            strings.append("hid.layer." + str(i))
+        strings.append("#outputs")
+        strings.append("#param.")
+        out_str = []
+        for i in self.dims:
+            out_str.append(i)
+        num_param = 0
+        for i in range(len(self.dims)-1):
+            num_param += self.dims[i]*self.dims[i+1] + 1
+        out_str.append(num_param)
+        for i in range(len(out_str)):
+            print('%-13s%-12i' % (strings[i], out_str[i]))
+        
+         #to visualize the weight of each neurons
+        if display_weights:
+            for l in range(self.n_hidden+1):
+                self.layers[l].display(l+1)
+        
 class Layer:
     def __init__(self, bias, nNeurons):
         self.bias = bias
         self.neurons = []
         for i in range(nNeurons):
             self.neurons.append(Neuron())
+    
+    def display(self, layer_num):
+        print('\nLayer %i parameters :' % layer_num)
+        print('Bias : %1.2f' % self.bias)
+        for n in range(len(self.neurons)):
+            print('Neuron %i weights :' % (n+1))
+            self.neurons[n].display()
 
 class Neuron:
     def __init__(self):
         self.weights = []
         
     def display(self):
-        for w in self.weights:
-            print(str(w) + "\n")
+        print(*["{0:0.2f}".format(i) for i in self.weights], sep = ", ")
         
 def main():
-    classifier = NN()
-	
+    classifier = NN((2,2,2), 1)
+    classifier.save('stuff')
+    #classifier = NN((1,1,1,1),2,'train',None,'stuff')
+    
+    classifier.display(True)
 	# Training
 	#classifier.train(dataset_train)
 	
